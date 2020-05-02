df083a80eb630af743f1214d2a4cfec476d5c5f6
hubfra <51798356+hubfra@users.noreply.github.com>
2020-05-02 10:50:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,28 @@
 
 #include <iostream>
 
-using namespace std;
-
-
-typedef unsigned long long ull;
-unsigned long long ModPow(ull base, ull exponent, ull mod);
+int64_t ModPow(int64_t base, int64_t exponent, int64_t modulus);
 
 
 int main()
 {
-	cout << "123^456 mod 789 is " << ModPow(123, 456, 789) << '\n';
-	cout << "19^1 mod 23 is " << ModPow(19, 1, 23) << '\n';
-	cout << "256^40 mod 100 is " << ModPow(256, 40, 100) << '\n';
-	cout << "4321^5678 mod 9876 is " << ModPow(4321, 5678, 9876) << '\n';
-	cout << "72^256 mod 99 is " << ModPow(72, 256, 99) << '\n';
-	cout << "784321^989125 mod 1356974152 is " << ModPow(784321, 989125, 1356974152) << '\n';
+	std::cout << "123^456 mod 789 is " << ModPow(123, 456, 789) << '\n';
+	std::cout << "19^1 mod 23 is " << ModPow(19, 1, 23) << '\n';
+	std::cout << "256^40 mod 100 is " << ModPow(256, 40, 100) << '\n';
+	std::cout << "4321^5678 mod 9876 is " << ModPow(4321, 5678, 9876) << '\n';
+	std::cout << "72^256 mod 99 is " << ModPow(72, 256, 99) << '\n';
+	std::cout << "784321^989125 mod 1356974152 is " << ModPow(784321, 989125, 1356974152) << '\n';
 
 	return 0;
 }
 
 
-ull ModPow(ull b, ull e, ull m)
+int64_t ModPow(int64_t b, int64_t e, int64_t m)
 {
-	ull result = 1;
-	ull pow = b % m;
+	int64_t result = 1;
+	int64_t pow = b % m;
 
-	for (ull a = 1; a <= e; a <<= 1) {
+	for (int64_t a = 1; a <= e; a <<= 1) {
 		if (a & e) {
 			result = (result * pow) % m;
 		}
